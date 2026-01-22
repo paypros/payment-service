14511c599f6d58d80ebb8168b5e6ed532c7c92cf
Ciberaccion <argranados@yahoo.com>
2026-01-22 17:35:20 -0600

    add springboot structure

---
+++ src/main/java/com/ciberaccion/paypro/PayproApplication.java
@@ -0,0 +1,13 @@
+package com.ciberaccion.paypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciberaccion/paypro/PayproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciberaccion.paypro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
